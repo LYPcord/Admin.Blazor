b108b6ea7829bfdc2b236c6ed94dca768f3b0252
7615163311 <761516331@qq.com>
2021-02-08 19:44:47 +0800

    第一次提交代码

---
+++ Admin.BBApp.WebAssembly/Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+using Admin.BBApp.Shared.Data;
+
+namespace Admin.BBApp.WebAssembly
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // 增加 BootstrapBlazor 组件
+            builder.Services.AddBootstrapBlazor();
+
+            builder.Services.AddSingleton<WeatherForecastService>();
+
+            var host = builder.Build();
+
+            await host.RunAsync();
+        }
+    }
+}
