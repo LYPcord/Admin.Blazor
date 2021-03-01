786267ccb1458f5a14740f793973f2a21bec4879
18211338136 <lingyanpei@quhulian.cn>
2021-03-01 19:16:19 +0800

    添加View页面

---
+++ src/Web/Admin.Blazor.Shared/Data/Admin/ViewService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+
+namespace Admin.Blazor.Shared.Data.Admin
+{
+    public class ViewService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly ILocalStorageService _localStorage;
+
+        public ViewService(HttpClient httpClient, ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+
+
+    }
+}
